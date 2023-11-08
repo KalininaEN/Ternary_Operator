aacdab2eb0e6add89473d4585443a1702e70bc7c
Homework1 <kalinina_en@hotmail.com>
2023-11-08 23:27:15 +0300

    Initial commit

---
+++ src/main/java/TernaryOperator.java
@@ -0,0 +1,15 @@
+public class TernaryOperator {
+    public static void main(String[] args) {
+        System.out.println(convertNumber(15));
+    }
+
+    public static String convertNumber(int number) {
+        String result;
+
+        result = number > 0 ? (number % 7 < 4 ? "positive-m" : "positive-p") : (number == 0 ? "zero" : (number % 7 > -4 ? "negative-m" : "negative-p"));
+
+        return result;
+    }
+}
+
+
