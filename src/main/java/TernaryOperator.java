681a964cf88d516089dd22e3a0bd3b7fa455dbc0
Homework1 <kalinina_en@hotmail.com>
2023-11-09 21:50:20 +0300

    delete variable result

--- src/main/java/TernaryOperator.java
+++ src/main/java/TernaryOperator.java
@@ -4,11 +4,7 @@ public class TernaryOperator {
     }
 
     public static String convertNumber(int number) {
-        String result;
-
-        result = number > 0 ? (number % 7 < 4 ? "positive-m" : "positive-p") : (number == 0 ? "zero" : (number % 7 > -4 ? "negative-m" : "negative-p"));
-
-        return result;
+        return number > 0 ? (number % 7 < 4 ? "positive-m" : "positive-p") : (number == 0 ? "zero" : (number % 7 > -4 ? "negative-m" : "negative-p"));
     }
 }
 
